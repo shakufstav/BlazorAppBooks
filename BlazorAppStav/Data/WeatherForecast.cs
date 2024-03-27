f39790340a88f1fb5aad17304656682cb855700c
Shahaf Shakuf <sshakuf@gmail.com>
2024-03-27 15:05:51 +0200

    Add project files.

---
+++ BlazorAppStav/Data/WeatherForecast.cs
@@ -0,0 +1,17 @@
+using API_service;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppStav.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+   
+}
